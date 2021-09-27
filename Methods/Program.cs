1ee4d82034b32cba457fd954be663ece1847b677
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-09-27 12:42:21 -0300

    Methods Notes

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -8,8 +8,49 @@ namespace Methods
 {
     class Program
     {
+        //declarations
+       static int score;
+        static string name;
+        static int exp;
+        //method that shows the HUD
+        static void ShowHUD()
+        {
+            //show hud:
+            Console.WriteLine("----------");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Exp: " + exp);
+            Console.WriteLine("----------");
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
+            //Methods
+            //(AKA procedures, functions, ...)
+
+
+            //declarations
+            name = "Arachne";
+
+            //...
+
+            //initialize
+
+
+            //hud
+            ShowHUD();
+
+            //simulated gameplay
+
+            score = score + 100;
+            exp = exp + 50;
+
+            //hud
+
+            ShowHUD();
+
+            Console.ReadKey(true);
         }
     }
 }
